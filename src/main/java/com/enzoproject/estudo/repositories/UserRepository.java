4235d40ac8b23d0b7d7143e5be0c3182256b7a21
Enzo Almeida <77026794+enzoalmeida21@users.noreply.github.com>
2023-11-17 19:07:57 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/enzoproject/estudo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.enzoproject.estudo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enzoproject.estudo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
